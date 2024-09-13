d52ff4a49a5f35b4d4225d5eb3f40e46158fd843
helix <bozardhh@hotmail.com>
2024-09-14 00:06:51 +0300

    :upd: hex colors to hsl

--- src/directives/tooltip.ts
+++ src/directives/tooltip.ts
@@ -1,12 +1,13 @@
 import type { FunctionDirective } from "vue";
+import styles from "./tooltip.module.css";
 
 const posTable = ["top", "bottom", "left", "right"];
 
 export const tooltip: FunctionDirective = (el, binding) => {
   el.setAttribute("data-tooltip", binding.value?.toString() || "");
-  el.classList.add("v-tooltip");
+  el.classList.add(styles["v-tooltip"]);
   const arg = binding.arg;
   if (arg && posTable.includes(arg)) {
-    el.classList.add("v-tooltip-" + binding.arg);
+    el.classList.add(styles["v-tooltip" + binding.arg]);
   }
 };
